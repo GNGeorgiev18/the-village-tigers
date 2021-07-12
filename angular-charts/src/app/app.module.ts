8c4ec31098b1179a7fc3233dde1366630ea15668
GNGeorgiev18 <58163283+GNGeorgiev18@users.noreply.github.com>
2021-07-12 23:47:03 +0300

    Update project

---
+++ angular-charts/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { ProductAreaLineChartsComponent } from './product-area-line-charts/product-area-line-charts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductAreaLineChartsComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgxChartsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-charts/src/app/data/products.ts
@@ -0,0 +1,58 @@
+export var productSalesMulti = [
+  {
+    "name": "planned start date",
+    "series": [
+      {
+        "name": "5th of July",
+        "value": 0
+      }, {
+        "name": "6th of July",
+        "value": 50
+      }
+    ]
+  }, {
+    "name": "real start date",
+    "series": [
+      {
+        "name": "6th of July",
+        "value": 50
+      }, {
+        "name": "7th of July",
+        "value": 100
+      }
+    ]
+  }, {
+    "name": "working process",
+    "series": [
+      {
+        "name": "7th of July",
+        "value": 100
+      }, {
+        "name": "14th of July",
+        "value": 100
+      }
+    ]
+  }, {
+    "name": "planned end date",
+    "series": [
+      {
+        "name": "14th of July",
+        "value": 100
+      }, {
+        "name": "15th of July",
+        "value": 50
+      }
+    ]
+  }, {
+    "name": "real end date",
+    "series": [
+      {
+        "name": "15th of July",
+        "value": 50
+      }, {
+        "name": "16th of July",
+        "value": 0
+      }
+    ]
+  }
+]
